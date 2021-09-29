8435419f79659e633f2c735e6a56d3e278a79f9c
Bentroen <bemcdc@gmail.com>
2021-09-29 04:48:37 -0300

    feat: add asset loader to grab tile images

---
+++ vvvvvv/map/tiles.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import zipfile
+
+source_path = "../data.zip"
+
+tilemaps = ["graphics/tiles.png", "graphics/tiles2.png", "graphics/tiles3.png"]
+
+
+class TileGrabber:
+    def __init__(self):
+        self._tilemaps = self._init_tilemaps()
+
+    def _init_tilemaps(self):
+        assets = AssetLoader()
+        for filename in tilemaps:
+            file = assets.load(filename)
+            img = Image.open(file)
+            self._tilemaps.append(img)
+
+    def get_tile(self, id: int, tileset: int) -> Image:
+        img = self._tilemaps[tileset]
+        x, y = divmod(id, img.width / 8)
+        return img.crop(x, y, x * 8, y * 8)
+
+
+# TODO: Move this class to a separate module in the future to unify data access
+class AssetLoader:
+    def __init__(self, path: str):
+        self._assets = self._init_assets(path)
+
+    def _init_assets(path: str) -> zipfile.ZipFile:
+        return zipfile.ZipFile(path, "r")
+
+    def load(self, filename: str):
+        return self._assets.read(filename)

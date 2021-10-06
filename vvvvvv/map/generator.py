e19c0577fe712deb40c0132b12703a6eb80863ae
Bentroen <bemcdc@gmail.com>
2021-10-06 02:47:23 -0300

    refactor: add MapAssembler class to build map image

--- vvvvvv/map/generator.py
+++ vvvvvv/map/generator.py
@@ -5,28 +5,63 @@ from PIL import Image
 import numpy as np
 
 
+CACHE_ROOMS = True
+CACHE_MAP = True
+
 rooms = fetch_map_data()
 tile_loader = TileGrabber()
 
 
-huge_map = Image.new("RGBA", (12800, 9600))
+class RoomGenerator:
+    pass
 
-for room_number, room in rooms.items():
-    tiles = np.array(room["tiles"])
 
-    map = Image.new("RGBA", (320, 240))
-    print(f"Processing room {room_number}")
+class MapAssembler:
+    """
+    Assembles room images from room tilemaps, tilesets and tile images.
+    """
 
-    for y, x in np.ndindex(tiles.shape):
-        id = int(tiles[y, x])
-        img = tile_loader.get_tile(id, room["tileset"])
+    def create_map_img(self, rooms: dict, rescale_factor: int = 8) -> Image:
+        """
+        Creates a large image containing all rooms. Only meant
+        to preview room placement; not used in asset generation.
+        """
 
-        map.paste(img, (x * 8, y * 8))
-    map.save(f".cache/rooms/{room_number}.png")
+        map_width = 320 * 79 // rescale_factor
+        map_height = 240 * 72 // rescale_factor
 
-    xx, yy = (int(x) for x in room_number.split(","))
-    map = map.resize((80, 60))
-    huge_map.paste(map, ((xx - 40) * 80, (yy - 40) * 60))
+        map_img = Image.new("RGBA", (map_width, map_height))
 
-huge_map = huge_map.crop(huge_map.getbbox())
-huge_map.save(".cache/huge_map.png")
+        for room_number, room in rooms.items():
+            print(f"Processing room {room_number}")
+            tiles = np.array(room["tiles"])
+            tileset = room["tileset"]
+            room_img = self.get_room_img(tiles, tileset)
+
+            if CACHE_ROOMS:
+                room_img.save(f".cache/rooms/{room_number}.png")
+
+            new_width = room_img.width // rescale_factor
+            new_height = room_img.height // rescale_factor
+
+            room_img = room_img.resize((new_width, new_height))
+            rx, ry = (int(x) for x in room_number.split(","))
+            map_img.paste(room_img, ((rx - 41) * new_width, (ry - 48) * new_height))
+
+        return map_img
+
+    def get_room_img(self, tiles: np.array, tileset: int) -> Image:
+        room = Image.new("RGBA", (320, 240))
+
+        for y, x in np.ndindex(tiles.shape):
+            id = int(tiles[y, x])
+            tile = tile_loader.get_tile(id, tileset)
+            room.paste(tile, (x * 8, y * 8))
+
+        return room
+
+
+if __name__ == "__main__":
+    map_builder = MapAssembler()
+    map = map_builder.create_map_img(rooms)
+    map.save(f".cache/map.png")

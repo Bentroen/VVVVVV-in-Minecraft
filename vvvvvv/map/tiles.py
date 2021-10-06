5ca81353f0caa3a5f7dcf6b91d0eb3bcefaaa8f3
Bentroen <bemcdc@gmail.com>
2021-10-06 02:48:44 -0300

    refactor: optimize tile lookup by pre-loading cropped tiles

--- vvvvvv/map/tiles.py
+++ vvvvvv/map/tiles.py
@@ -9,19 +9,20 @@ tilemaps = ["graphics/tiles.png", "graphics/tiles2.png", "graphics/tiles3.png"]
 
 class TileGrabber:
     def __init__(self):
-        self._tilemaps = []
-        self._init_tilemaps()
+        self._tiles = []
+        self._init_tiles()
 
-    def _init_tilemaps(self):
+    def _init_tiles(self):
         loader = AssetLoader()
         for file in tilemaps:
             img = loader.load_img(file)
-            self._tilemaps.append(img)
+            for y in range(img.height // 8):
+                for x in range(img.width // 8):
+                    tile = img.crop((x * 8, y * 8, x * 8 + 8, y * 8 + 8))
+                    self._tiles.append(tile)
 
     def get_tile(self, id: int, tileset: int) -> Image:
-        img = self._tilemaps[tileset]
-        y, x = divmod(id, img.width / 8)
-        return img.crop((x * 8, y * 8, x * 8 + 8, y * 8 + 8))
+        return self._tiles[tileset * 1200 + id]
 
 
 # TODO: Move this class to a separate module in the future to unify data access

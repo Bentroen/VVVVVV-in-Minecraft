9e6b7e64d00231ff3ef5da5c22a7fa1d72706be2
Bentroen <bemcdc@gmail.com>
2021-10-08 02:15:30 -0300

    refactor: split asset generators into different classes
    It's a bit shady to use classes to "group" a bunch of functions as classmethods, but it works fine and is a tidy way of keeping all the "Minecraft bits" into a single module.

--- vvvvvv/map/generate.py
+++ vvvvvv/map/generate.py
@@ -20,100 +20,105 @@ def beet_default(ctx: Context):
 
     rooms = map_data.fetch_map_data()
 
-    ### MODELS/TEXTURES
-
-    image = MapAssembler(rooms)
-    multipart = get_base_multipart()
-
-    sliced_rooms = image.slice_rooms(10)
-
-    for room_count, (room_number, slices) in enumerate(sliced_rooms.items()):
-        rx, ry = tuple(int(x) for x in room_number.split(","))
-
-        for slice_count, slice in enumerate(slices):
-            id = f"{rx}_{ry}_{slice_count}"
-            filename = f"vvvvvv:rooms/{id}"
-            ctx.assets[filename] = Texture(slice)
-            ctx.assets[filename] = Model(get_base_model(filename))
-
-            cmd = room_count * 12 + slice_count + 1
-            multipart["overrides"].append(get_predicate(cmd, filename))
-
-            # ctx.data[filename + ".mcfunction"] = Function([lines], tags=["minecraft:load"])
-
-    ctx.assets["minecraft:item/diamond_hoe"] = Model(multipart)
-
-    ### FUNCTIONS
-
-    # Collision maps
-
-    max_height = max(
-        map_data.get_room_stack_position(*tuple(int(x) for x in room.split(",")))[1]
-        for room in rooms
-    )
-
-    width = 40  # room width
-    height = max_height + 1  # height of tallest stack
-    length = 30 * 3 + 2  # length of three rooms stacked with a single block gap
-
-    blocks = np.zeros((length, height, width))
-
-    for room_number, room in rooms.items():
-        rx, ry = tuple(int(x) for x in room_number.split(","))
-        tiles = room["tiles"]
-        tileset = room["tileset"]
-
-        stack, index = map_data.get_room_stack_position(rx, ry)
-
-        for i, row in enumerate(tiles):
-            for j, tile in enumerate(row):
-
-                tile_type = map_data.tile_type(tileset, tile)
-
-                if tile_type == "wall":
-                    block = 1
-                elif tile_type == "spike":
-                    block = 2
-                elif tile_type == "background":
-                    block = 3
-
-                x = stack * 31 + (29 - i)
-                y = index
-                z = j
-
-                blocks[x, y, z] = block
-
-    ctx.data["vvvvvv:map_collision"] = get_collision_structure(blocks)
+    ModelGenerator.generate(rooms, ctx)
+    CollisionGenerator.generate(rooms, ctx)
 
 
-def get_base_model(texture: str) -> dict:
-    return {"parent": "vvvvvv:base/10x10", "textures": {"texture": texture}}
+class ModelGenerator:
+    @classmethod
+    def generate(cls, rooms: dict, ctx: Context) -> None:
+        image = MapAssembler(rooms)
+        sliced_rooms = image.slice_rooms(10)
+
+        multipart = cls.get_multipart_base()
+
+        for room_count, (room_number, slices) in enumerate(sliced_rooms.items()):
+            rx, ry = tuple(int(x) for x in room_number.split(","))
+
+            for slice_count, slice in enumerate(slices):
+                id = f"{rx}_{ry}_{slice_count}"
+                filename = f"vvvvvv:rooms/{id}"
+                ctx.assets[filename] = Texture(slice)
+                ctx.assets[filename] = Model(cls.get_base_model(filename))
+
+                cmd = room_count * 12 + slice_count + 1
+                multipart["overrides"].append(
+                    cls.get_multipart_predicate(cmd, filename)
+                )
+
+        ctx.assets["minecraft:item/diamond_hoe"] = Model(multipart)
+
+    @classmethod
+    def get_base_model(cls, texture: str) -> dict:
+        return {"parent": "vvvvvv:base/10x10", "textures": {"texture": texture}}
+
+    @classmethod
+    def get_multipart_base(cls) -> dict:
+        return {"parent": "vvvvvv:base/10x10", "overrides": []}
+
+    @classmethod
+    def get_multipart_predicate(cls, cmd: int, model: str) -> dict:
+        return {"predicate": {"custom_model_data": cmd}, "model": model}
 
 
-def get_base_multipart() -> dict:
-    return {"parent": "vvvvvv:base/10x10", "overrides": []}
+class CollisionGenerator:
+    @classmethod
+    def generate(cls, rooms: dict, ctx: Context) -> None:
+        max_height = max(
+            map_data.get_room_stack_position(*tuple(int(x) for x in room.split(",")))[1]
+            for room in rooms
+        )
 
+        width = 40  # room width
+        height = max_height + 1  # height of tallest stack
+        length = 30 * 3 + 2  # length of three rooms stacked with a single block gap
 
-def get_predicate(cmd: int, model: str) -> dict:
-    return {"predicate": {"custom_model_data": cmd}, "model": model}
+        blocks = np.zeros((length, height, width))
 
+        for room_number, room in rooms.items():
+            rx, ry = tuple(int(x) for x in room_number.split(","))
+            tiles = room["tiles"]
+            tileset = room["tileset"]
 
-def get_collision_structure(blocks: np.array) -> Structure:
-    """Return a `Structure` for the blocks in the input array."""
+            stack, index = map_data.get_room_stack_position(rx, ry)
 
-    return Structure(
-        {
-            "DataVersion": tag.Int(2730),  # 1.17.1
-            "size": [tag.Int(x) for x in blocks.shape],
-            "palette": [
-                {"Name": "minecraft:air"},
-                {"Name": "minecraft:black_concrete"},
-                {"Name": "minecraft:red_concrete"},
-                {"Name": "minecraft:white_concrete"},
-            ],
-            "blocks": [
-                {"pos": coords, "state": tag.Int(block)}
-                for coords, block in np.ndenumerate(blocks)
-            ],
-        }
-    )
+            for i, row in enumerate(tiles):
+                for j, tile in enumerate(row):
+
+                    tile_type = map_data.tile_type(tileset, tile)
+
+                    if tile_type == "wall":
+                        block = 1
+                    elif tile_type == "spike":
+                        block = 2
+                    elif tile_type == "background":
+                        block = 3
+
+                    x = stack * 31 + (29 - i)
+                    y = index
+                    z = j
+
+                    blocks[x, y, z] = block
+
+        ctx.data["vvvvvv:map_collision"] = cls.get_structure(blocks)
+
+    @classmethod
+    def get_structure(cls, blocks: np.array) -> Structure:
+        """Return a `Structure` for the blocks in the input array."""
+
+        return Structure(
+            {
+                "DataVersion": tag.Int(2730),  # 1.17.1
+                "size": [tag.Int(x) for x in blocks.shape],
+                "palette": [
+                    {"Name": "minecraft:air"},
+                    {"Name": "minecraft:black_concrete"},
+                    {"Name": "minecraft:red_concrete"},
+                    {"Name": "minecraft:white_concrete"},
+                ],
+                "blocks": [
+                    {"pos": coords, "state": tag.Int(block)}
+                    for coords, block in np.ndenumerate(blocks)
+                ],
+            }
+        )

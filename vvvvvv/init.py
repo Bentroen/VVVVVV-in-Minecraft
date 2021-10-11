08097acc9a2c47d41d4214ecc4b0968018135810
Bentroen <bemcdc@gmail.com>
2021-10-11 04:25:35 -0300

    feat: clear snow layer model for player platform

--- vvvvvv/init.py
+++ vvvvvv/init.py
@@ -1,4 +1,4 @@
-from beet import Context, FunctionTag
+from beet import Context, FunctionTag, Model
 from PIL import Image
 
 
@@ -6,3 +6,7 @@ def beet_default(ctx: Context):
     # Set up tick and load functions
     ctx.data["minecraft:load"] = FunctionTag({"values": ["vvvvvv:load"]})
     ctx.data["minecraft:tick"] = FunctionTag({"values": ["vvvvvv:tick"]})
+
+    # Clear snow layer model for player platform
+    ctx.assets["minecraft:block/snow_height14"] = Model({"elements": []})
+

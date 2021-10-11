9233a9a1e123f6e8b96b5f4b2a38199760fb31c8
Bentroen <bemcdc@gmail.com>
2021-10-11 04:53:24 -0300

    feat: add pack icons

--- vvvvvv/init.py
+++ vvvvvv/init.py
@@ -1,8 +1,14 @@
-from beet import Context, FunctionTag, Model
+from beet import Context, PngFile, FunctionTag, Model
 from PIL import Image
+from .utils.file import AssetLoader
 
 
 def beet_default(ctx: Context):
+    # Add pack icons
+    icon = AssetLoader().load_img("VVVVVV.png")
+    ctx.data.icon = PngFile(icon)
+    ctx.assets.icon = PngFile(icon)
+
     # Set up tick and load functions
     ctx.data["minecraft:load"] = FunctionTag({"values": ["vvvvvv:load"]})
     ctx.data["minecraft:tick"] = FunctionTag({"values": ["vvvvvv:tick"]})
